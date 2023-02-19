44ca13b8b3c47bb4dcdd94486a5fd67e8fce23d0
HYUNJUNEPARK <june2ac4dev@gmail.com>
2023-02-20 08:39:38 +0900

    tmp: 함수를 파라미터로 넘겨 동작 성공

--- app/src/main/java/com/study/customex/AlertDialogManager.kt
+++ app/src/main/java/com/study/customex/AlertDialogManager.kt
@@ -1,9 +1,20 @@
 package com.study.customex
 
+import android.app.AlertDialog
+import android.app.Dialog
 import android.content.Context
-import androidx.appcompat.app.AlertDialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.LinearLayout
+import androidx.appcompat.widget.AppCompatButton
 
-class AlertDialogManager(private val context: Context) {
+class AlertDialogManager(
+    private val context: Context,
+    private val parameterYFunction: () -> Unit,
+    ): View.OnClickListener {
     /**
      *
      */
@@ -17,12 +28,57 @@ class AlertDialogManager(private val context: Context) {
             .setTitle(title)
             .setMessage(message)
             .setPositiveButton(positiveButtonText) { _, int ->
+                Log.d("testLog", "testFun: inner")
                 //Toast.makeText(context, "INT : $int", Toast.LENGTH_SHORT).show()
-                //positiveFunc
+                parameterYFunction()
+
             }
             .setNegativeButton(negativeButtonText) { dialogInterface, _ ->
                 dialogInterface.dismiss()
             }
             .show()
     }
+    private val dialog: Dialog
+    private var view: View
+
+    init {
+        dialog = object : Dialog(context, R.style.Theme_Dialog) {
+
+            override fun onBackPressed() {
+                super.onBackPressed()
+            }
+
+            override fun dismiss() {
+                super.dismiss()
+            }
+        }
+        val lp = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.MATCH_PARENT
+        )
+        view = buildView(context)
+        dialog.setContentView(view, lp)
+        //set transparent to see the rounded corners
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+    }
+
+    fun showDialog() {
+        dialog.show()
+    }
+
+    private fun buildView(context: Context): View {
+        val inflater = LayoutInflater.from(context)
+        val view = inflater.inflate(R.layout.custom_sample_dialog_01, null)
+        (view.findViewById(R.id.btnPositive) as AppCompatButton).setOnClickListener {
+            dialog.dismiss()
+            parameterYFunction
+
+        }
+        return view
+    }
+
+    override fun onClick(v: View?) {
+        //TODO("Not yet implemented")
+    }
+
 }
--- app/src/main/java/com/study/customex/MainActivity.kt
+++ app/src/main/java/com/study/customex/MainActivity.kt
@@ -3,6 +3,7 @@ package com.study.customex
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.widget.TextView
 import android.widget.Toast
@@ -13,71 +14,85 @@ import com.study.customex.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var alertDialogManager: AlertDialogManager
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.mainActivity = this
 
-        alertDialogManager = AlertDialogManager(this)
+
     }
 
     //Ex1
     fun onEx1() {
-        Dialog(this).apply {
-            setContentView(R.layout.custom_sample_dialog_01)
-            this.findViewById<TextView>(R.id.titleTextView).text = "Ex1 AlertDialog"
-            this.findViewById<TextView>(R.id.contentTextView).text = "Alert Contents"
-
-            this.findViewById<AppCompatButton>(R.id.btnNegative).apply {
-                text = "N"
-                setOnClickListener {
-                    dismiss()
-                }
-            }
-
-            this.findViewById<AppCompatButton>(R.id.btnPositive).apply {
-                text = "Y"
-                setOnClickListener {
-                    Toast.makeText(context, "positive button", Toast.LENGTH_SHORT).show()
-                }
-            }
-            show()
-        }
+//        Dialog(this).apply {
+//            setContentView(R.layout.custom_sample_dialog_01)
+//            this.findViewById<TextView>(R.id.titleTextView).text = "Ex1 AlertDialog"
+//            this.findViewById<TextView>(R.id.contentTextView).text = "Alert Contents"
+//
+//            this.findViewById<AppCompatButton>(R.id.btnNegative).apply {
+//                text = "N"
+//                setOnClickListener {
+//                    dismiss()
+//                }
+//            }
+//
+//            this.findViewById<AppCompatButton>(R.id.btnPositive).apply {
+//                text = "Y"
+//                setOnClickListener {
+//                    Toast.makeText(context, "positive button", Toast.LENGTH_SHORT).show()
+//                }
+//            }
+//            show()
+//        }
     }
 
     //Ex4
     fun onEx2() {
-        val view = LayoutInflater.from(this).inflate(R.layout.custom_sample_dialog_02, null)
-        view.findViewById<TextView>(R.id.titleTextView).text = "Ex3 AlertDialog"
-        val alertDialog = AlertDialog.Builder(this, R.style.CustomAlertDialog)
-            .setView(view)
-            .setCancelable(false)
-            .create()
-        view.findViewById<AppCompatButton>(R.id.btnPositive).apply {
-            this.text = "Y"
-            this.setOnClickListener {
-                Toast.makeText(context, "positive button", Toast.LENGTH_SHORT).show()
-            }
-        }
-        view.findViewById<AppCompatButton>(R.id.btnNegative).apply {
-            this.text = "N"
-            this.setOnClickListener {
-                alertDialog.dismiss()
-            }
-        }
-        alertDialog.show()
+//        val view = LayoutInflater.from(this).inflate(R.layout.custom_sample_dialog_02, null)
+//        view.findViewById<TextView>(R.id.titleTextView).text = "Ex3 AlertDialog"
+//        val alertDialog = AlertDialog.Builder(this, R.style.CustomAlertDialog)
+//            .setView(view)
+//            .setCancelable(false)
+//            .create()
+//        view.findViewById<AppCompatButton>(R.id.btnPositive2).apply {
+//            this.text = "Y"
+//            this.setOnClickListener {
+//                Toast.makeText(context, "positive button", Toast.LENGTH_SHORT).show()
+//            }
+//        }
+//        view.findViewById<AppCompatButton>(R.id.btnNegative2).apply {
+//            this.text = "N"
+//            this.setOnClickListener {
+//                alertDialog.dismiss()
+//            }
+//        }
+//        alertDialog.show()
+        AlertDialogManager(this, ::testFun).showPlainAlertDialog(
+            "aaa",
+            "aaaadf",
+            "asdsf",
+            "asdfdsfasfs"
+        )
+
     }
 
     //Ex4
     fun onEx3() {
-        alertDialogManager.showPlainAlertDialog(
-            title = "title",
-            message = "message",
-            positiveButtonText = "Y",
-            negativeButtonText = "N"
-        )
+//        alertDialogManager.showPlainAlertDialog(
+//            title = "title",
+//            message = "message",
+//            positiveButtonText = "Y",
+//            negativeButtonText = "N"
+//        )
+        Log.d("testLog", "testFun: ==========")
+        AlertDialogManager(this, ::testFun).showDialog()
+
+
+        //alertDialogManager.showDialog()
+    }
+
+    fun testFun() {
+        Log.d("testLog", "testFun: 123123")
     }
 }
